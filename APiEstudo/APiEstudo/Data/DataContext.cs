99e4047ebf858f6a8f680ffb98f5dd84b0d6bf21
Cleberton <clebertonfgc@gmail.com>
2021-10-07 17:31:30 -0300

    Finalizando config db

---
+++ APiEstudo/APiEstudo/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using APiEstudo.Models;
+using Microsoft.EntityFrameworkCore;
+
+// Faz o mapeamento das classes da pasta models para o Entity criar o banco e as tabelas
+namespace APiEstudo.Data
+{
+    public class DataContext : DbContext
+    {
+        // colocando no construtor as options do banco de dados recebidas no startup
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<Produtos> Produtos { get; set; }
+    }
+}
--- APiEstudo/APiEstudo/Startup.cs
+++ APiEstudo/APiEstudo/Startup.cs
@@ -1,7 +1,9 @@
+using APiEstudo.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,6 +27,10 @@ namespace APiEstudo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Adcionando a referencia do banco de dados para a API
+            services.AddDbContext<DataContext>(
+                context => context.UseSqlite(Configuration.GetConnectionString("Default"))
+            );
             services.AddControllers();
         }
 
