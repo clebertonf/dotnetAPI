3acefad04e26dee2f535a0574de5e55bb30a1b20
Cleberton <clebertonfgc@gmail.com>
2021-10-07 16:45:36 -0300

    Parte 1 Básica da API

---
+++ APiEstudo/APiEstudo/Controllers/ProdutosController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace APiEstudo.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class ProdutosController : ControllerBase
+    {
+        // GET: api/<ProdutosController>
+        [HttpGet("produtos")]
+        public ActionResult Get()
+        {
+            return Ok("Ola mundo");
+        }
+    }
+}
---
+++ APiEstudo/APiEstudo/Models/Produtos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APiEstudo.Models
+{
+    public class Produtos
+    {
+        public Produtos() { }
+
+        public Produtos(int id, string nome, string descricao)
+        {
+            Id = id;
+            Nome = nome;
+            Descricao = descricao;
+        }
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+    }
+}
