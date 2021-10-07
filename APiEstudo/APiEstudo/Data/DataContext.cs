1a296e9c4b1945481b299a4d3dd12d1bbd412d04
Cleberton <clebertonfgc@gmail.com>
2021-10-07 18:08:20 -0300

    Populando banco automaticamente

--- APiEstudo/APiEstudo/Data/DataContext.cs
+++ APiEstudo/APiEstudo/Data/DataContext.cs
@@ -1,5 +1,6 @@
 ﻿using APiEstudo.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 
 // Faz o mapeamento das classes da pasta models para o Entity criar o banco e as tabelas
 namespace APiEstudo.Data
@@ -9,5 +10,25 @@ namespace APiEstudo.Data
         // colocando no construtor as options do banco de dados recebidas no startup
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
         public DbSet<Produtos> Produtos { get; set; }
+
+        // Método que popula o banco de dados
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Produtos>()
+                .HasData(new List<Produtos>(){
+                    new Produtos(1, "Desktop", "I5 256 SSD 16GB Ram"),
+                    new Produtos(2, "Samsung", "Galaxy M30"),
+                    new Produtos(3, "Iphone Apple", "Iphone 13"),
+                    new Produtos(4, "smart tv", "Smart 55 polegadas"),
+                    new Produtos(5, "Mouse del", "Mouse del ergonômico"),
+                });
+        }
     }
 }
+
+
+// Comandos para criar o banco com EntityFrameworkCore
+
+// get-help entityframeworkcore (mostra todos comando possiveis)
+// 01 - Add-Migration (cria as migrations)
+// 02 - Update-Database (executa as migrations)
---
+++ APiEstudo/APiEstudo/Migrations/20211007210550_init.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace APiEstudo.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Produtos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(nullable: true),
+                    Descricao = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produtos", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Produtos",
+                columns: new[] { "Id", "Descricao", "Nome" },
+                values: new object[] { 1, "I5 256 SSD 16GB Ram", "Desktop" });
+
+            migrationBuilder.InsertData(
+                table: "Produtos",
+                columns: new[] { "Id", "Descricao", "Nome" },
+                values: new object[] { 2, "Galaxy M30", "Samsung" });
+
+            migrationBuilder.InsertData(
+                table: "Produtos",
+                columns: new[] { "Id", "Descricao", "Nome" },
+                values: new object[] { 3, "Iphone 13", "Iphone Apple" });
+
+            migrationBuilder.InsertData(
+                table: "Produtos",
+                columns: new[] { "Id", "Descricao", "Nome" },
+                values: new object[] { 4, "Smart 55 polegadas", "smart tv" });
+
+            migrationBuilder.InsertData(
+                table: "Produtos",
+                columns: new[] { "Id", "Descricao", "Nome" },
+                values: new object[] { 5, "Mouse del ergonômico", "Mouse del" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produtos");
+        }
+    }
+}
