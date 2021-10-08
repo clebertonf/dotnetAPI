cd97594302347e004047d8168d2e24b8593d5c6f
Cleberton <clebertonfgc@gmail.com>
2021-10-08 13:26:54 -0300

    Metodos get/put/post controllers

--- APiEstudo/APiEstudo/Controllers/ProdutosController.cs
+++ APiEstudo/APiEstudo/Controllers/ProdutosController.cs
@@ -1,4 +1,5 @@
 ﻿using APiEstudo.Data;
+using APiEstudo.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -18,11 +19,45 @@ namespace APiEstudo.Controllers
             _context = context;
         }
 
-        // GET: api/<ProdutosController>
+        // GET: api/produtos
         [HttpGet("produtos")]
         public ActionResult Get()
         {
             return Ok(_context.Produtos);
+        }
+
+        // GET/ID: api/produtos/id
+        [HttpGet("produtos/{id}")]
+        public ActionResult GetId(int id)
+        {
+            var message = new List<object>() { "Object not found " };
+            var product = _context.Produtos.FirstOrDefault(product => product.Id == id);
+            if (product == null) return BadRequest(message);
+            return Ok(product);
+        }
+
+        // POST: api/produtos/post
+        [HttpPost("produtos/new")]
+        public ActionResult CreateProduct([FromBody] Produtos product)
+        {
+            try
+            {
+                _context.Produtos.Add(product);
+                _context.SaveChanges();
+                return Ok("Product insert Sucess!");
+
+            } catch
+            {
+                return BadRequest("Houve um erro");
+            };
+           
+        }
+
+        // PUT: api/produtos/put
+        [HttpPut("produtos/ed")]
+        public ActionResult Edit()
+        {
+            return Ok();
         }
     }
 }
